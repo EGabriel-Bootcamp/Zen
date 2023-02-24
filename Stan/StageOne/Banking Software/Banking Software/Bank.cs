07be5ece9357bc61034e5ac9376dd6afcccdb20d
stanleyjoe33 <stanleyojoseph33@gmail.com>
2023-02-24 20:12:36 +0100

    Added credential validator class

--- Stan/StageOne/Banking Software/Banking Software/Bank.cs
+++ Stan/StageOne/Banking Software/Banking Software/Bank.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
  
 
 namespace Banking_Software
@@ -52,7 +53,7 @@ namespace Banking_Software
 
 
             //creating a validator object "checker"
-            Validator checker = new Validator();
+            FieldValidator checker = new FieldValidator();
 
             //using the field_validator method of the checker object to validate all the fields with && operator
 
@@ -65,12 +66,18 @@ namespace Banking_Software
 
 
 
-            //recursion conditional
+            //Conditional to take user to login page if all fields are valid or perform recursion if any field is invalid
             if ( allValid == true)
             {
-                //implement the saving to text file here
+                //saving credentials to text file
+               string passwordPath = @"savedPassword.txt";
+               File.WriteAllText(passwordPath, password);
 
-                //implementing the Login method to take the user to the login page
+                string usernamePath = @"savedUsername.txt";
+                File.WriteAllText(usernamePath, password);
+
+
+                //Calling the Login method to take the user to the login page
                 Login(attempts);
             }
             else
@@ -86,29 +93,84 @@ namespace Banking_Software
 
         public void Login( int attempts )
         {
-            //n should define the number of attempts so far
-            //think of using recursion for the limited nummber of attempts implementation
-            // ?do this by settin a 
-
+             
             Console.WriteLine("LOGIN \n" +
                 $"PLEASE NOTE THAT YOU HAVE {attempts} ATTEMPTS LEFT!");
 
+            //collecting visitor credentials
             Console.WriteLine("Input your Username");
-            string customer_username = Console.ReadLine();
+            string visitorUsername = Console.ReadLine();
 
             Console.WriteLine("input your password");
-            string customer_password = Console.ReadLine();
+            string visitorPassword = Console.ReadLine();
 
-            if (attempts != 0 && true /*read and validate credential*/)
-                {
+            // Reading saved credentials from txt file
+            string savedPassword = System.IO.File.ReadAllText(@"savedPassword.txt");
+            string savedUsername = System.IO.File.ReadAllText(@"savedUsername.txt");
+
+            //comparing credentials
+            bool cleared;
+
+            if (visitorUsername == savedUsername && visitorPassword == savedPassword)
+            {
+                cleared = true;
+            }
+            else
+            {
+                Console.WriteLine("Invalid Username or Passwor \n" +
+                    "Try again");
+                cleared = false;
+                Login (attempts);
+            }
+            //Final login Verification and intended operation operation selection
+            if (attempts != 0 && cleared == true)
+            {
                 Console.WriteLine("LOGIN SUCCESSFUL!");
-                Console.WriteLine( "WELCOME" + username + " How can we help you today? \n" +
+                Console.WriteLine("WELCOME " + username + " How can we help you today? \n" +
                     "Reply:\n" +
                     " 1 for Deposit \n" +
                     "2 for withdrawal \n" +
                     "3 to view balance \n" +
                     "4 to view account summary");
+                string loginRequest = Console.ReadLine();
+
+
+
+                //Defining the method for login operation that would recall itself when a user makes an invalid input
+                void LoginOperation()
+                {
+                    if (loginRequest == "1")
+                    {
+                        Deposit();
+                    }
+                    else if (loginRequest == "2")
+                    {
+                        //implement withdrawal
+                        Withdraw();
+                    }
+                    else if (loginRequest == "3")
+                    {
+                        ViewBalance();
+                    }
+                    else if (loginRequest == "4")
+                    {
+                        ViewAccountSummary();
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("You did not select a valid input");
+                        LoginOperation();
+
+                    }
                 }
+
+
+
+                //calling the method
+                LoginOperation();
+                 
+            }
 
 
 
@@ -118,10 +180,7 @@ namespace Banking_Software
 
                 Console.WriteLine("Maximum number of attempts exceeded!");
 
-                if (customer_username == "" && customer_password == "")
-                {
-
-                }
+                Console.WriteLine(exitMessage);
             }
             else
             {
@@ -143,7 +202,7 @@ namespace Banking_Software
             //Validating user input
             bool isDepositValid = int.TryParse( depositInput, out int depositAmount );
 
-            if (isDepositValid == true)
+            if (isDepositValid == true && depositAmount >= 0 && depositAmount != 0)
             {
 
                 accountBalance = accountBalance + depositAmount;
@@ -151,6 +210,7 @@ namespace Banking_Software
                 Console.WriteLine("Repy \n" +
                     " 1 to deposit again.\n" +
                     "2 to withdraw \n" +
+                    "3 to view your balance" +
                     "press any key to exit");
                 string depositReply = Console.ReadLine();
                 
@@ -161,8 +221,9 @@ namespace Banking_Software
                 }
                 else if (depositReply == "2")
                 {
-                    //implement withdrawal
+                    Withdraw();
                 }
+                
                 else
                 {
                     Console.WriteLine(exitMessage);
@@ -173,21 +234,77 @@ namespace Banking_Software
                 //if the input amount is not valid the user would be prompted
                 //And the deposit method would be called again
 
-                Console.WriteLine("Please input a valid non-negative amount");
+                Console.WriteLine("Please input a valid non-negative integer for: Deposit amount");
                 Deposit();
             }
 
         }
-        public void viewBalance()
+
+        public void Withdraw()
+        {
+
+            //collecting withdrawal input
+            Console.WriteLine("How much do you wish to withdraw");
+            string withdrawalInput = Console.ReadLine();
+
+            //Validating user input
+            bool isWithdrawalValid = int.TryParse(withdrawalInput, out int withdrawnAmount);
+
+            if (isWithdrawalValid == true && withdrawnAmount >= 0 && withdrawnAmount != 0)
+            {
+
+                accountBalance = accountBalance - withdrawnAmount;
+                Console.WriteLine($"Your withdrawal of: {withdrawnAmount} was successful");
+                Console.WriteLine("Repy \n" +
+                    " 1 to withdraw again.\n" +
+                    "2 to deposit \n" +
+                    "3 to view your balance" +
+                    "press any key to exit");
+                string withdrawReply = Console.ReadLine();
+
+                //conditional to withdraw again, deposit or exit
+                if (withdrawReply == "1")
+                {
+                    Withdraw();
+                }
+                else if (withdrawReply == "2")
+                {
+                    Deposit();
+                }
+
+                else
+                {
+                    Console.WriteLine(exitMessage);
+                }
+
+            }
+            else
+            {
+
+                //if the input amount is not valid the user would be prompted
+                //And the withdraw method would be called again
+
+                Console.WriteLine("Please input a valid non-negative Integer for :Withdrawal amount");
+                Withdraw();
+            }
+        }
+        public void ViewBalance()
         {
             //creating the balance check time
             DateTime timeObject = DateTime.Now;
             string time = timeObject.ToString();
-            
+
 
             Console.WriteLine($"your account balance as of {time} is:");
             Console.WriteLine(accountBalance);
         }
+
+
+        public void ViewAccountSummary()
+        {
+
+        }
+   
 
     }
 
--- Stan/StageOne/Banking Software/Banking Software/FieldValidator.cs
+++ Stan/StageOne/Banking Software/Banking Software/FieldValidator.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Banking_Software
 {
-    public class Validator
+    public class FieldValidator
     {
         public bool field_validator(string x)
         {
